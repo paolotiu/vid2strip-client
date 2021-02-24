41e6b3201500d8b0af5b148f92357b001f703381
Paolo Tiu <paolotiu17@gmail.com>
2021-02-24 16:12:51 +0800

    styled-components and ui lib

---
+++ next.config.js
@@ -0,0 +1,14 @@
+//const withTM = require('next-transpile-modules')(['@babylonjs']);
+const withTM = require("next-transpile-modules")(["tiu-ui"]); // As per comment.
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
+  target: "serverless",
+  webpack: function (config) {
+    /// below is not required for the problem described. Just for reference.(es6)
+    config.module.rules.push({ test: /\.yml$/, use: "raw-loader" });
+    return config;
+  },
+};
+
+module.exports = withPlugins([withTM], nextConfig);
