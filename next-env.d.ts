7dbabff29ddacd7cbf5477ada4567f0a4ddcb94b
Paolo Tiu <paolotiu17@gmail.com>
2021-02-28 17:31:41 +0800

    added svg

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module "*.svg" {
+  import { ReactElement, SVGProps } from "react";
+  const content: (props: SVGProps<SVGElement>) => ReactElement;
+  export default content;
+}
