f9b583968982bca1706f547669e24f02aa3f3bf9
Paolo Tiu <paolotiu17@gmail.com>
2021-03-04 16:12:51 +0800

    comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     setSocket(socket);
 
     return () => {
+      // Close socket one user closes website
       socket.close();
     };
   }, []);
